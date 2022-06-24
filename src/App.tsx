7197e7c4cfc5b6453e9648a7bd1c25e58646b57a
Willjpg <willianf.silva@outlook.com>
2022-06-23 22:51:50 -0300

    Aula 03-1/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Event } from "./pages/Event"
+import { Router } from "./Router"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
 
 
 function App() {
   
   return (
   <div>
-    <Event/>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+     </ApolloProvider>
 
   </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,6 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-     <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
